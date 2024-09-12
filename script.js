8580959469dc8317912eb450b96bc0e04e7d4beb
giannisaladino <saladinogianni6@gmail.com>
2024-09-12 17:58:49 +0200

    ADD: added kart's position 0 and the possibility of move the kart

--- script.js
+++ script.js
@@ -3,6 +3,13 @@
 // FASE DI PREPARAZIONE
 // RACCOLGO GLI ELEMENTI DI INTERESSE DALLA PAGINA
 
+// bottone sinistra
+const leftButton = document.querySelector('.left-button');
+
+// bottone destra
+const rightButton = document.querySelector('.right-button');
+
+// griglia
 const grid = document.querySelector('.grid');
 
 // Prepariamo la griglia iniziale
@@ -23,9 +30,12 @@ const gridMatrix = [
 // impostazioni iniziali
 const kartPosition = { y: 7, x: 3 };
 
-//   FUNZIONI RELATIVE ALLA GRIGLIA
+// FUNZIONI RELATIVE ALLA GRIGLIA
 // funzione per renderizzare la griglia
 function renderGrid() {
+    // prima di tutto svuota la griglia
+    grid.innerHTML = '';
+
     // recupero ogni riga della matrice
     gridMatrix.forEach(function(rowCells){
         // console.log(rowCells);
@@ -55,11 +65,66 @@ function placeKart() {
     gridMatrix[kartPosition.y][kartPosition.x] = 'kart';
 }
 
+// funzione per muovere il kart
+function moveKart(direction){
+    // console.log(direction);
+
+    // 'solleviamo' il kart per spostarlo da un'altra parte
+    gridMatrix[kartPosition.y][kartPosition.x] = '';
+
+
+    // aggiorniamo le coordinate a seconda della direzione
+    switch (direction) {
+        case 'left':
+            if (kartPosition.x > 0) {
+                kartPosition.x--
+            } 
+            break;
+        case 'right':
+            if(kartPosition.x < 6) {
+                kartPosition.x++
+            }
+            break;  
+            default:
+                gridMatrix[kartPosition.y][kartPosition.x] = 'kart';
+    }
+
+    // rirenderizzare tutti gli elementi
+    renderElements();
+}
+
 // FUNZIONE CHE RAGGRUPPA LE OPERAZIONI DI RENDERING
-function renderElement(){
+function renderElements(){
     placeKart();
     renderGrid();
 }
 
+// EVENTI DI GIOCO
+// click bottone sinistra
+leftButton.addEventListener('click', function(){
+    moveKart('left');
+})
+
+// click bottone destra
+rightButton.addEventListener('click', function(){
+    moveKart('right');
+});
+
+// reazione alle freccette
+document.addEventListener('keyup', function(event){
+    // console.log(event.key);
+    switch(event.key) {
+        case 'ArrowLeft':
+            moveKart('left');
+            break;
+        case 'ArrowRight':
+            moveKart('right');
+            break;
+        default: return;
+    }      
+});
+
+
+
 // esecuzione delle funzioni di gioco
-renderElement();
+renderElements();
